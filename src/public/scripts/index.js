afef2e6736ba52c55df09901cdf5355325385849
Ramon <ramon.taino@icloud.com>
2017-01-14 15:36:34 -0600

    Install YoutTube Api and create videoSearch

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -2,27 +2,33 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import '../css/styles.css';
 
+import config from './config/config';
+import YouTubeApiSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 
-class App extends Component {
 
+class App extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
             videos: []
         };
-
-        this.videoSearch('React Development');
+        this.videoSearch(config.DEFAULT_SEARCH_TERM);
     }
 
     videoSearch(term) {
-        console.log(term);
+        YouTubeApiSearch({
+            key: config.YOUTUBE_API_KEY,
+            term: term
+        },(videos => this.setState({videos})))
     }
 
     render() {
         return (
             <div className="container">
                <SearchBar onSearchChange={term => this.videoSearch(term)}/>
+               Videos Count: {this.state.videos.length}
             </div>
         );
     }
